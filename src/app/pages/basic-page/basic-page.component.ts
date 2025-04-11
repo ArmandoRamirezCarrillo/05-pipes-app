e48b116074a55c251e37916fbf2829213ae16a55
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2025-04-11 14:07:50 -0600

    Creando pipes de fechas

---
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,36 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.component.html',
+})
+export default class BasicPageComponent {
+
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
+  nameLower  = signal('armando');
+  nameUpper  = signal('ARMANDO');
+  fullName = signal('ArmAndo RaMirEz CARriLlO');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    })
+
+  });
+
+  changeLocale(locale: AvailableLocale){
+    this.localeService.changeLocale(locale);
+  }
+
+}
---
+++ src/app/services/locale.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, signal } from '@angular/core';
+
+export type AvailableLocale = 'en' | 'fr' | 'es'
+
+@Injectable({providedIn: 'root'})
+export class LocaleService {
+  private currentLocale = signal<AvailableLocale>('es');
+
+  constructor(){
+    (this.currentLocale.set(localStorage.getItem('locale') as AvailableLocale) ?? 'es');
+  }
+
+  get getLocale(){
+    return this.currentLocale();
+  }
+
+  changeLocale(locale: AvailableLocale){
+    localStorage.setItem('locale', locale);
+    this.currentLocale.set(locale);
+    window.location.reload();
+  }
+
+}
